ff993b045cda3db7bde524806dc4bd1179b8b76c
seongo <seongo0521@gmail.com>
2023-02-16 20:55:48 +0900

    수정 - Article Entity 관계 수정

--- src/main/java/com/fintech/fintech/domain/Article.java
+++ src/main/java/com/fintech/fintech/domain/Article.java
@@ -10,7 +10,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 // 게시글
 @Getter
@@ -32,6 +34,12 @@ public class Article {
     @Setter @Column(nullable = false, length = 10000) private String content; // 내용
 
     @Setter private String hashtag; // 해시태그
+
+    @ToString.Exclude // articleComment 에도 ToString이 존재하기 때문에 순환참조가 일어나게 되므로 ToString을 끊어준다.
+    // foreign key 를 설정할때는 여러가지 제약사항이 생길 수 있으므로 설계시 고려해야 할 사항을 파악해야 함.
+    @OrderBy("id") // 정렬기준 설정
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // mappedBy 를 설정해주지 않으면 default 가 두 Entity 이름을 합쳐서 생성되기 때문에 설정해준다.
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 다른 방법도 사용이 가능하지만 중복 허용을 하지 않기때문에 Set 으로 처리해준다.
 
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
